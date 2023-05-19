80b79162e8c77b4b0886b3f3fbd2bece7f0ad8c0
Andrey <mr.dallas007@gmail.com>
2023-05-19 17:12:09 +0300

    Adding button event processors

--- app/src/main/java/com/example/pr2_gurdzhii/MainActivity.java
+++ app/src/main/java/com/example/pr2_gurdzhii/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.pr2_gurdzhii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +17,20 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.wc1);
         text.setText("11111111111");
 
-        ImageView image = (TextView) findViewById(R.id.Image1);
-        image.setImageDrawable(getResources().getDrawable(R.drawable.book_svgrepo_com));
+        ImageView image = (ImageView) findViewById(R.id.Image1);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher_background));
+
+        //        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                Log.i(TAG,"Button has been pressed");
+//            }
+//        });
+
+    }
+    public void buttonDeclare(View view){
+        Log.i(TAG,"Button has been pressed again");
+    }
     }
 }
