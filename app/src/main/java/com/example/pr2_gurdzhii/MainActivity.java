783f1e16ff30c36781bbfc530f720294a21f1efc
Andrey <mr.dallas007@gmail.com>
2023-05-19 17:08:24 +0300

    Initialize resources in code

--- app/src/main/java/com/example/pr2_gurdzhii/MainActivity.java
+++ app/src/main/java/com/example/pr2_gurdzhii/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pr2_gurdzhii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.wc1);
+        text.setText("11111111111");
+
+        ImageView image = (TextView) findViewById(R.id.Image1);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.book_svgrepo_com));
     }
 }
